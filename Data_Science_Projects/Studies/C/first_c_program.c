f7d74f92ed998847f9bcf2fd30884f1ba6f76e5a
evelyncosta00 <evelyncoding92@gmail.com>
2024-03-07 21:09:04 -0300

    adicionando arquivo c

---
+++ Data_Science_Projects/Studies/C/first_c_program.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int main() {
+    printf("Olá, mundo!\n");
+    return 0;
+}
+
+
+
+-----------------------------------------------------
+
+
+
+#include <stdio.h>
+
+int soma_vinte(int n) {
+    return n + 20;
+}
+
+int main() {
+    printf("%d\n", soma_vinte(5));
+    return 0;
+}
+
+
+
+-----------------------------------------------------------
+
+
+
+#include <stdio.h>
+
+double calcula_salario_dia(double salary) {
+    return salary / 30;
+}
+
+int main() {
+    printf("%.2f\n", calcula_salario_dia(1412));
+    return 0;
+}
+
+
+
+-----------------------------------------------------------
+
+
+
+#include <stdio.h>
+
+double calcula_media(double a, double b) {
+    return (a + b) / 2;
+}
+
+int main() {
+    printf("%.2f\n", calcula_media(10, 20));
+    return 0;
+}
+
+
+
+-----------------------------------------------------------
+
+
+
+
+
